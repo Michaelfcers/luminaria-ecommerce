af211587353c268ec90ae225fa9d8575536d6eaa
Michael Rodriguez Cervantes <146405926+Michaelfcers@users.noreply.github.com>
2025-11-23 23:48:05 -0500

    Cambios en inicio

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,7 +1,5 @@
 import { HeroSection } from "@/features/store/components/hero-section"
 import { BrandsSection } from "@/features/store/components/brands-section"
-import { CategoriesSection } from "@/features/store/components/categories-section"
-import { FeaturedProducts } from "@/features/store/components/featured-products"
 import { PromotionsCarousel } from "@/features/store/components/promotions-carousel"
 
 export default function HomePage() {
@@ -10,8 +8,6 @@ export default function HomePage() {
       <HeroSection />
       <PromotionsCarousel />
       <BrandsSection />
-      <CategoriesSection />
-      <FeaturedProducts />
     </>
   )
 }
